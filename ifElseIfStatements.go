7dc18dac77cb814d1a8e0f1f0fc23d302ad5746b
Alexander Wong <alexandthemachine@gmail.com>
2022-03-29 11:51:19 +0800

    Using oddOrEven and noOfDigits to check the numbers.

--- ifElseIfStatements.go
+++ ifElseIfStatements.go
@@ -1,6 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
 func main() {
+
 	//Insert your code here
+	number := -100
 	//Hint if a:= ?? ; condition {  }
+
+	oddOrEven(number)
+	noOfDigits(number)
+}
+
+func oddOrEven(number int) {
+	if number%2 == 0 {
+		fmt.Printf("%v"+" is even.\n", number)
+	} else if number%2 == 1 {
+		fmt.Printf("%v"+" is odd. \n", number)
+	}
+}
+
+func noOfDigits(number int) {
+	number = int(math.Abs(float64(number)))
+	numToString := strconv.Itoa(number)
+	lengthOfString := len(numToString)
+	fmt.Printf("Number of digits : %v.\n", lengthOfString)
 }
